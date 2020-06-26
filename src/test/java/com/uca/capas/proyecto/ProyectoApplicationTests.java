47fabd9bed57e454e245c54cee1c6395484a49ca
Cristian Mundo <00086917@uca.edu.sv>
2020-06-25 21:18:16 -0600

    creacion del proyecto

---
+++ src/test/java/com/uca/capas/proyecto/ProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.capas.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
