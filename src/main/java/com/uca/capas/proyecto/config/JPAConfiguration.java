5ca76edff80653501a31a79272957af1a64285f0
DavidVallecios <00078217@uca.edu.sv>
2020-06-26 22:24:04 -0600

    base jpa

--- src/main/java/com/uca/capas/proyecto/config/JPAConfiguration.java
+++ src/main/java/com/uca/capas/proyecto/config/JPAConfiguration.java
@@ -37,9 +37,9 @@ public class JPAConfiguration {
         DriverManagerDataSource dataSource = new DriverManagerDataSource();
         dataSource.setDriverClassName("org.postgresql.Driver");
 
-        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/<nombre de nuestra BD>");
+        dataSource.setUrl("jdbc:postgresql://127.0.0.1:5432/finalCapas");
         dataSource.setUsername("postgres");
-        dataSource.setPassword("<su contrasenia>");
+        dataSource.setPassword("ncapas");
 
         return dataSource;
     }
