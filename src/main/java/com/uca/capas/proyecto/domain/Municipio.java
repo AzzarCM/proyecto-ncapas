e015331823250d07d1530be8526cd354fc1ccf6b
Cristian Mundo <00086917@uca.edu.sv>
2020-06-27 20:15:41 -0600

    entidades

---
+++ src/main/java/com/uca/capas/proyecto/domain/Municipio.java
@@ -0,0 +1,46 @@
+package com.uca.capas.proyecto.domain;
+
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(schema = "public", name = "municipio")
+public class Municipio {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_municipio")
+    private Integer idMunicipio;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @OneToMany(mappedBy="municipio",fetch= FetchType.EAGER)
+    private List<CatalogoCE> catalogoCEList;
+
+    public Integer getIdMunicipio() {
+        return idMunicipio;
+    }
+
+    public void setIdMunicipio(Integer idMunicipio) {
+        this.idMunicipio = idMunicipio;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public List<CatalogoCE> getCatalogoCEList() {
+        return catalogoCEList;
+    }
+
+    public void setCatalogoCEList(List<CatalogoCE> catalogoCEList) {
+        this.catalogoCEList = catalogoCEList;
+    }
+
+    //private Departamento departamento;
+}
